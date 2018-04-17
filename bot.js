efd7f1726b5c4f62621060e590e8c367e244a5c7
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-04-17 21:27:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,51 +1,28 @@
 const Discord = require('discord.js');
-const Fortnite = require('fortnite');
-const stats = new Fortnite(process.env.TRN);
-
-
+const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on('message', message => {
+  if(message.author.id !== "349616310734553088") return;
+  const prefix = "-";
+  const args = message.content.split(" ").slice(1);
 
-exports.run = (client, message, args, tools) => {
+  if (message.content.startsWith(prefix + "eval")) {
+    try {
+      var code = args.join(" ");
+      var evaled = eval(code);
 
-  let platform;
-  let username;
-  if (-['pc','xbl','psn'].includes(args[0])) return message.channel.send('**Please Include the platform: `-fortnite [ pc | xbl | psn ] <username>`**');
-  if (!args[1]) return message.channel.send('**Please Include the username: `-fortnite [ pc | xbl | psn ] <username>`**');
- 
-  // Assign Values
-  platform = args.shift(); 
-    username = args.join(' ');
-  stats.getInfo(username, platform).then( data => { 
-   
-    const embed = new Discord.MessageEmbed() 
-      .setColor(0xffffff) 
-      .setTitle(`Stats for ${data.username}`)
-      .setDescription(`**Top Placement**\n\n**Top 3s:** *${data.lifetimeStats[0].value}*\n**Top 5s:** *${data.lifetimeStats[1].value}*\n**Top 6s:** *${data.lifetimeStats[3].value}*\n**Top 12s:** *${data.lifetimeStats[4].value}*\n**Top 25s:** *${data.lifetimeStats[5].value}*`, true) // We can have other information look different, in fields or in the description.
-      .setThumbnail('https://vignette.wikia.nocookie.net/fortnite/images/d/d8/Icon_Founders_Badge.png') 
-      .addField('Total Score', data.lifetimeStats[6].value, true)
-      .addField('Matches Played', data.lifetimeStats[7].value, true)
-      .addField('Wins', data.lifetimeStats[8].value, true)
-      .addField('Win Percentage', data.lifetimeStats[9].value, true)
-      .addField('Kills', data.lifetimeStats[10].value, true)
-      .addField('K/D Ratio', data.lifetimeStats[11].value, true)
-      .addField('Kills Per Minute', data.lifetimeStats[12].value, true)
-      .addField('Time Played', data.lifetimeStats[13].value, true)
-      .addField('Average Survival Time', data.lifetimeStats[14].value, true)
-   
-    message.channel.send(embed)
-   
-  })
-  .catch(error => {
-   
-    message.channel.send('Username not found!');
- 
-  })
- 
-}
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.sendCode("xl", clean(evaled));
+    } catch (err) {
+      message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
