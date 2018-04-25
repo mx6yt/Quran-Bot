0f34bc8936ed1b0c33827752dc1f65be19b93a6a
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-04-25 18:09:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,9 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const ytdl = require('ytdl-core');
-const request = require('request');
-const fs = require('fs');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-
-const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const discord = new Discord.Client();
+const Music = require('discord.js-musicbot-addon');
+const music = new Music(client, {
+    youtubeKey: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4'
+  });
 const prefix = '-';
 client.on('ready', function() {
 	console.log(`i am ready ${client.user.username}`);
