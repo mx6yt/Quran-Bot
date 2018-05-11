a45c6b5370410f9228b82fb50120589e1c16dc94
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-11 20:00:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,50 +26,6 @@ const music = new Music(client, {
     enableQueueStat: true,
   });
 
-var aoasm =[
-    {q:"Quran **ما أول ما نزل من التوراة**",a:"بسم"},
-
-    
-    
-    
-   ];
-   client.on("message", async message => {
-	   var prefix = "*";
-    if(message.content == prefix+"اسئلة"){
-
-        UserBlocked.add(message.guild.id)
-        var ask = aoasm[Math.floor(Math.random() * aoasm.length)];
-        let embed = new Discord.RichEmbed()
-        .setTitle('عاصمة')
-        .setAuthor(message.author.username, message.author.avatarURL)
-        .setColor("RANDOM")
-        .setDescription(ask.q);
-        message.channel.sendEmbed(embed).then(msg=> msg.delete(20000))
-        const msgs = await message.channel.awaitMessages(msg => msg.author.id !== client.user.id ,{maxMatches:1,time:10000});
-            UserBlocked.delete(message.guild.id)
-        msgs.forEach(result => {
-           if(result.author.id == client.user.id) return;
-           if(result.content == "اسئلة") return
-           if(result.content == ask.a){
-
-             let embeds = new Discord.RichEmbed()
-             .setTitle(':white_check_mark: اجابة صحيحة')
-             .setAuthor(message.author.username, message.author.avatarURL)
-             .setColor("RANDOM")
-             .setDescription(`**${result.author.username}** الإجابة صحيحة`);
-                message.channel.sendEmbed(embeds);                return;
-           } else {
-
-                                  var embedx = new Discord.RichEmbed()
-                .setTitle(':x:خطاء')
-                .setAuthor(message.author.username, message.author.avatarURL)
-                .setColor("RANDOM")
-                .setDescription(`**${result.author.username}** الإجابة خاطئة`);
-                message.channel.sendEmbed(embedx);
-           }
-     });
-	     }
-     });
 	    
 client.on('message', message => {
   if(message.content === "Q**bot") {
